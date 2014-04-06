75e7810d5e655b48bef87b4bb6a2640ee007e50a
Rafal 'bl4de' Janicki <bloorq@gmail.com>
2014-04-06 22:41:54 +0200

    Jquery added for Bootstrap

--- web/index.php
+++ web/index.php
@@ -1,70 +1,93 @@
 <?php
 require_once '../vendor/autoload.php';
-$app = new Application("App");
-$appInstance = $app->getInstance();
+$app=new Application( "App");
+$appInstance=$app->getInstance();
 ?>
+
 <!DOCTYPE html>
-    <html lang="en">
-        <head>
-        <title>Framework test app</title>
-        <link rel="stylesheet" type="text/css" href="styles/bootstrap.min.css" />
-        <link rel="stylesheet" href="styles/application.css" />
-    </head>
-        <body ng-app="Application">
-            <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+<html lang="en">
+
+<head>
+    <title>Framework test app</title>
+    <link rel="stylesheet" type="text/css" href="styles/bootstrap.min.css" />
+    <link rel="stylesheet" href="styles/application.css" />
+</head>
+
+<body ng-app="Application" ng-controller="ApplicationController">
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">BB Learning Platform</a>
-                </div>
-                    <div class="navbar-collapse collapse">
-                        <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#">Dashboard</a></li>
-                        <li><a href="#">Settings</a></li>
-                        <li><a href="#">Profile</a></li>
-                        <li><a href="#">Help</a></li>
-                    </ul>
-                        <form class="navbar-form navbar-right">
-                        <input type="text" class="form-control" placeholder="Search...">
-                    </form>
-                </div>
+                </button>
+                <a class="navbar-brand" href="#">BB Learning Platform</a>
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="#">Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="#">Settings</a>
+                    </li>
+                    <li>
+                        <a href="#">Profile</a>
+                    </li>
+                    <li>
+                        <a href="#">Help</a>
+                    </li>
+                </ul>
+                <form class="navbar-form navbar-right">
+                    <input type="text" class="form-control" placeholder="Search...">
+                </form>
             </div>
         </div>
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-sm-3 col-md-2 sidebar">
-                        <ul class="nav nav-sidebar">
-                        <li class="active"><a href="#">Dashboard</a></li>
-                        <li><a href="#">Settings</a></li>
-                        <li><a href="#">Profile</a></li>
-                        <li><a href="#">Messages</a></li>
-                    </ul>
-                    
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-3 col-md-2 sidebar">
+                <ul class="nav nav-sidebar">
+                    <li class="active">
+                        <a href="#">Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="#">Settings</a>
+                    </li>
+                    <li>
+                        <a href="#">Profile</a>
+                    </li>
+                    <li>
+                        <a href="#">Messages</a>
+                    </li>
+                </ul>
+
+            </div>
+            <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+                <h2 class="page-header">Dashboard</h2>
+                <div class="container-fluid">
+                    <div class="row">
+                        <p class="col-md-6">
+                            {{ hello }}<i class="glyphicon glyphicon-search"></i>
+                        </p>
+                        <p class="col-md-6">{{ newHello }}</p>
+                    </div>
                 </div>
-                    <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-                    <h2 class="page-header">Dashboard</h2>
-                        <div class="container-fluid">
-                            <div class="row">
-                                <p class="col-md-6"><i class="glyphicon glyphicon-search"></i></p>
-                                <p class="col-md-6">test</p>
-                            </div>
-                        </div>
-                    <h4 class="sub-header">Section title</h4>
-                    
-                </div>
+                <h4 class="sub-header">Section title</h4>
+
             </div>
         </div>
-        
-        <!-- vendors -->
-        <script src="vendor/angular.min.js"></script>
-        <script src="vendor/bootstrap.min.js"></script>
-        <!-- application -->
-        <script src="scripts/application.js"></script>
-        
-    </body>
+    </div>
+
+    <!-- vendors -->
+    <script src="vendor/jquery.min.js"></script>
+    <script src="vendor/angular.min.js"></script>
+    <script src="vendor/bootstrap.min.js"></script>
+    <!-- application -->
+    <script src="scripts/application.js"></script>
+
+</body>
+
 </html>
--- web/scripts/application.js
+++ web/scripts/application.js
@@ -1,9 +1,14 @@
 /*
  * main JS application script - runner
  */
+
+/* global angular */
 var Application = angular.module("Application", []);
 
-Application.controller("ApplicationController", [ "$scope", function($scope) {
+Application.controller("ApplicationController", ["$scope",
+    function ($scope) {
         $scope.hello = "Hello!";
-}]);
 
+        $scope.newHello = "New hello!!!";
+    }
+]);
