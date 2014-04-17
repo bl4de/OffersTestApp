40751735d463b711ae1bbb4e3176dc427a2e54ac
Rafal 'bl4de' Janicki <bloorq@gmail.com>
2014-04-17 06:31:02 +0200

    Application changes

--- application.php
+++ application.php
@@ -9,11 +9,11 @@ require 'vendor/rb.phar';
 $app = new Slim\Slim();
 
 class ObjectMaper {
-    public static function map($obj) {
+    public static function map( $obj ) {
         $arr = [];
 
-        foreach ($obj as $key => $value) {
-            array_push($arr, [
+        foreach ( $obj as $key => $value ) {
+            array_push( $arr, [
                 "id" => $value->id,
                 "company" => $value->company,
                 "country" => $value->country,
@@ -22,28 +22,28 @@ class ObjectMaper {
                 "position" => $value->position,
                 "added" => $value->added,
                 "link" => $value->link
-            ]);
+                ] );
         }
         return $arr;
     }
 }
 
 
-R::setup('mysql:host=localhost;dbname=offers','root','root');
+R::setup( 'mysql:host=localhost;dbname=offers', 'root', 'root' );
 
-$app->get("/offer/:id", function($id) use ($app) {
-    $offer = R::load('offer',$id);
-    $app->response()->header('Content-Type', 'application/json');
-    echo json_encode($offer);
-});
+$app->get( "/offer/:id", function( $id ) use ( $app ) {
+        $offer = R::load( 'offer', $id );
+        $app->response()->header( 'Content-Type', 'application/json' );
+        echo json_encode( $offer );
+    } );
 
-$app->get("/offers", function() use ($app) {
-    $offers = R::findAll('offer');
-    // print_r($offers);
+$app->get( "/offers", function() use ( $app ) {
+        $offers = R::findAll( 'offer' );
+        // print_r($offers);
 
-    $app->response()->header('Content-Type', 'application/json');
-    echo json_encode( ["offers" => ObjectMaper::map($offers) ]);
-});
+        $app->response()->header( 'Content-Type', 'application/json' );
+        echo json_encode( ["offers" => ObjectMaper::map( $offers ) ] );
+    } );
 
 $app->run();
 R::close();
